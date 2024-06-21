ea8ce3124e43803e161a45383d77245f21bf6e30
NirmalKumarPandey <nirmalnirman36@gmail.com>
2024-06-21 17:48:37 +0530

    remove http://localhost:4567 Every where

--- Demo/src/Component/Login.js
+++ Demo/src/Component/Login.js
@@ -18,7 +18,7 @@ function Login() {
             method: "POST",
             body: dataToSend,
         }
-        let JSONData = await fetch("http://localhost:4567/validateToken", reqOption);
+        let JSONData = await fetch("/validateToken", reqOption);
         let JSOData = await JSONData.json();
         console.log(JSOData);
         // Copy this code...................
@@ -40,7 +40,7 @@ function Login() {
             method: "POST",
             body: dataToSend,
         }
-        let JSONData = await fetch("http://localhost:4567/login", reqOption);
+        let JSONData = await fetch("/login", reqOption);
         let JsoData = await JSONData.json();
         if (JsoData.status === "success") {
             // Want to store credential into local Storage
